f05a07c8f6ca19218a61ab4c2dfe15f514bc2c64
Misterh100 <handsomenyathi1@gmail.com>
2026-02-11 23:43:29 +0200

    feat: add tutorial mode on first launch

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import { storeData } from "/src/storage.js";
 import { loadBanner } from "/src/banner.js";
 import { isNewDay } from "/src/util.js";
 import { getToday } from "/src/time.js";
+import { showTutorial } from "/src/tutorial.js";
+let state = JSON.parse(localStorage.getItem("appstate"));
 
-let appState = JSON.parse(localStorage.getItem("appstate"));
-
-if (!appState) {
+if (!state) {
   let numArray = Array.from({ length: 100 }, (_, i) => i + 1);
   let activeArray = numArray.slice(0, 6);
-  appState = {
+  state = {
     numArray: numArray.slice(6, 100),
     currentArray: activeArray,
     selectedArray: [],
@@ -20,13 +20,17 @@ if (!appState) {
     currentStreak: 0,
     completeDays: 0,
     lastSave: null,
+    firstLaunch: true,
   };
 
-  storeData("appstate", appState);
+  storeData("appstate", state);
 }
 
 function loadApp() {
-  let state = JSON.parse(localStorage.getItem("appstate"));
+  state = JSON.parse(localStorage.getItem("appstate"));
+  if (state.firstLaunch) {
+    showTutorial();
+  }
   if (state.lastSave != null) {
     let today = getToday();
     if (isNewDay(today, state.lastSave)) {
---
+++ src/tutorialData.js
@@ -0,0 +1,19 @@
+const tutorialData = {
+  banner: {
+    name: "Banner",
+    tutorial:
+      "Banner displays: Date, Completed Days, Total Saved in %, Missed Days",
+  },
+  wheel: {
+    name: "Wheel",
+    tutorial:
+      "Wheel Display amounts in Rands. Tap on amount to select. A maximum of 2 amount can be selected per day. If two Amounts are selected they will add up to form a total Amount",
+  },
+  submit: {
+    name: "Submit",
+    tutorial:
+      "Submit the total amount selected. Submition confirms that the total amount is to be stashed (BE HONEST)",
+  },
+};
+
+export default tutorialData;
